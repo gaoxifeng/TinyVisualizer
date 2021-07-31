473f8c11e1c249eef6a171fe476ebb38f84c0c71
pzr1988 <zherong@cs.unc.edu>
2021-07-30 19:55:38 -0500

    better initial view setup

--- TinyVisualizer/CameraManipulator.cpp
+++ TinyVisualizer/CameraManipulator.cpp
@@ -4,9 +4,12 @@
 namespace DRAWER {
 CameraManipulator::CameraManipulator(Camera3D& camera):_camera(camera) {}
 void CameraManipulator::init(GLFWwindow* wnd,const Eigen::Matrix<GLfloat,6,1>& bb) {
-  if(!std::isfinite(bb[0]) || std::abs(bb[0])==std::numeric_limits<GLfloat>::max())
-    _camera.setPosition(Eigen::Matrix<GLfloat,3,1>(-1,0,0));
-  else _camera.setPosition(Eigen::Matrix<GLfloat,3,1>(bb[0],(bb[1]+bb[4])/2,(bb[2]+bb[5])/2));
-  _camera.setDirection(Eigen::Matrix<GLfloat,3,1>(1,0,0));
+  if(!std::isfinite(bb[0]) || std::abs(bb[0])==std::numeric_limits<GLfloat>::max()) {
+    _camera.setPosition(Eigen::Matrix<GLfloat,3,1>(1,1,1));
+    _camera.setDirection(Eigen::Matrix<GLfloat,3,1>(-1,-1,-1));
+  } else {
+    _camera.setPosition(bb.template segment<3>(3)*1.5f-bb.template segment<3>(0)*0.5f);
+    _camera.setDirection(bb.template segment<3>(0)-bb.template segment<3>(3));
+  }
 }
 }
