a4b007708b51e522e25493a5c9d0937516ad3014
pzr1988 <zherong@cs.unc.edu>
2021-08-06 03:13:15 -0500

    installed imgui

--- cmake/FindTinyVisualizer.cmake
+++ cmake/FindTinyVisualizer.cmake
@@ -10,6 +10,16 @@ FIND_PATH(TinyVisualizer_INCLUDE_DIR TinyVisualizer/Drawer.h
   C:/TinyVisualizer/include
   )
 
+FIND_PATH(ImGui_INCLUDE_DIR imgui/imgui.h
+  ${PROJECT_SOURCE_DIR}/include
+  ${PROJECT_SOURCE_DIR}
+  $ENV{TinyVisualizer_ROOT}/include
+  $ENV{TinyVisualizer_ROOT}
+  /usr/include
+  /usr/local/include
+  C:/TinyVisualizer/include
+  )
+
 FIND_LIBRARY(TinyVisualizer_LIBRARIES NAMES TinyVisualizer glfw3 PATHS 
   ${PROJECT_SOURCE_DIR}/lib
   ${PROJECT_SOURCE_DIR}
@@ -22,7 +32,7 @@ FIND_LIBRARY(TinyVisualizer_LIBRARIES NAMES TinyVisualizer glfw3 PATHS
 
 IF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
   set(TinyVisualizer_FOUND TRUE)
-  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR})
+  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR} ${ImGui_INCLUDE_DIR})
 ENDIF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
 
 MARK_AS_ADVANCED(TinyVisualizer_INCLUDE_DIR TinyVisualizer_INCLUDE_DIRS TinyVisualizer_LIBRARIES)
