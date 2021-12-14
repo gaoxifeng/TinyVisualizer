efab1d1f64ddddcda40c55b9023bd52ad9bf7df1
zrpan <zrpan@tencent.com>
2021-12-14 08:11:58 +0800

    updated cmake for windows

--- cmake/FindTinyVisualizer.cmake
+++ cmake/FindTinyVisualizer.cmake
@@ -3,36 +3,53 @@ IF(NOT TinyVisualizer_FOUND)
 FIND_PATH(TinyVisualizer_INCLUDE_DIR TinyVisualizer/Drawer.h
   ${PROJECT_SOURCE_DIR}/include
   ${PROJECT_SOURCE_DIR}
-  $ENV{TinyVisualizer_ROOT}/include
-  $ENV{TinyVisualizer_ROOT}
   /usr/include
   /usr/local/include
-  C:/TinyVisualizer/include
+  ${TinyVisualizer_ROOT}/include
+  ${TinyVisualizer_ROOT}
   )
 
 FIND_PATH(ImGui_INCLUDE_DIR imgui/imgui.h
+  ${PROJECT_SOURCE_DIR}/extern
   ${PROJECT_SOURCE_DIR}/include
   ${PROJECT_SOURCE_DIR}
-  $ENV{TinyVisualizer_ROOT}/include
-  $ENV{TinyVisualizer_ROOT}
   /usr/include
   /usr/local/include
-  C:/TinyVisualizer/include
+  ${TinyVisualizer_ROOT}/extern
+  ${TinyVisualizer_ROOT}/include
+  ${TinyVisualizer_ROOT}
   )
 
-FIND_LIBRARY(TinyVisualizer_LIBRARIES NAMES TinyVisualizer glfw3 PATHS 
+FIND_PATH(glad_INCLUDE_DIR glad/gl.h
+  ${PROJECT_SOURCE_DIR}/extern/glad/include
+  ${PROJECT_SOURCE_DIR}/extern/glad
+  ${PROJECT_SOURCE_DIR}/include
+  ${PROJECT_SOURCE_DIR}
+  /usr/include
+  /usr/local/include
+  ${TinyVisualizer_ROOT}/extern/glad/include
+  ${TinyVisualizer_ROOT}/extern/glad
+  ${TinyVisualizer_ROOT}/include
+  ${TinyVisualizer_ROOT}
+  )
+
+FIND_LIBRARY(TinyVisualizer_LIBRARIES NAME TinyVisualizer PATHS 
+  ${PROJECT_SOURCE_DIR}/Release
   ${PROJECT_SOURCE_DIR}/lib
   ${PROJECT_SOURCE_DIR}
-  $ENV{TinyVisualizer_ROOT}/lib
-  $ENV{TinyVisualizer_ROOT}
   /usr/lib
   /usr/local/lib
-  C:/TinyVisualizer/lib
+  ${TinyVisualizer_ROOT}/Release
+  ${TinyVisualizer_ROOT}/lib
+  ${TinyVisualizer_ROOT}
+  ${TinyVisualizer_ROOT}-build/Release
+  ${TinyVisualizer_ROOT}-build/lib
+  ${TinyVisualizer_ROOT}-build
   )
 
 IF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
   set(TinyVisualizer_FOUND TRUE)
-  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR} ${ImGui_INCLUDE_DIR})
+  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR} ${ImGui_INCLUDE_DIR} ${glad_INCLUDE_DIR})
 ENDIF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
 
 MARK_AS_ADVANCED(TinyVisualizer_INCLUDE_DIR TinyVisualizer_INCLUDE_DIRS TinyVisualizer_LIBRARIES)
---
+++ commit.bat
@@ -0,0 +1,4 @@
+DEL*.user
+git add --all
+git commit -m %1
+git push -u origin %2
