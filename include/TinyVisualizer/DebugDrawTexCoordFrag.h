a01ad748b00d4f69bf22b981414ca6ddf63694f3
zrpan <zrpan@tencent.com>
2023-02-06 17:21:58 -0800

    added numpy dependency

--- include/TinyVisualizer/DebugDrawTexCoordFrag.h
+++
@@ -1,8 +0,0 @@
-const std::string DebugDrawTexCoordFrag=
-  "#version 330 core\n"
-  "in vec2 tc;\n"
-  "out vec4 FragColor;\n"
-  "void main(void)\n"
-  "{\n"
-  "  FragColor=vec4(tc,0,1);\n"
-  "}\n";
