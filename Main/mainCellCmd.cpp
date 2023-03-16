b2537721c3c3e5861a46ca8a2037b2ee1293ec3f
zherong <zherong@cs.unc.edu>
2023-03-16 12:17:09 -0700

    updated camera exporter, fixed bug in redundant output

---
+++ Main/mainCellCmd.cpp
@@ -0,0 +1,110 @@
+#include <TinyVisualizer/Drawer.h>
+#include <TinyVisualizer/CellShape.h>
+#include <TinyVisualizer/ShadowAndLight.h>
+
+using namespace DRAWER;
+
+int main(int argc,char** argv) {
+  Drawer drawer(argc,argv);
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    for(int x=0; x<3; x++)
+      ids.push_back(Eigen::Matrix<int,3,1>(x,0,0));
+    for(int y=0; y<3; y++)
+      ids.push_back(Eigen::Matrix<int,3,1>(0,y,0));
+    for(int z=0; z<3; z++)
+      ids.push_back(Eigen::Matrix<int,3,1>(0,0,z));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("tripod.obj");
+  }
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    for(int x=-2; x<=2; x++)
+      ids.push_back(Eigen::Matrix<int,3,1>(x,0,0));
+    for(int y=-2; y<=2; y++)
+      ids.push_back(Eigen::Matrix<int,3,1>(0,y,0));
+    for(int z=-2; z<=2; z++)
+      ids.push_back(Eigen::Matrix<int,3,1>(0,0,z));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("octpod.obj");
+  }
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    ids.push_back(Eigen::Matrix<int,3,1>(0,0,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(2,0,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(0,2,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(2,2,0));
+    for(int x=0; x<=2; x++)
+      for(int y=0; y<=2; y++)
+        ids.push_back(Eigen::Matrix<int,3,1>(x,y,1));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("walker.obj");
+  }
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    for(int z=0; z<=1; z++) {
+      ids.push_back(Eigen::Matrix<int,3,1>(0,0,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(2,0,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(0,2,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(2,2,z));
+    }
+    for(int x=0; x<=2; x++)
+      for(int y=0; y<=2; y++)
+        ids.push_back(Eigen::Matrix<int,3,1>(x,y,2));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("walkerHigh.obj");
+  }
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    ids.push_back(Eigen::Matrix<int,3,1>(0,0,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(3,0,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(0,3,0));
+    ids.push_back(Eigen::Matrix<int,3,1>(3,3,0));
+    for(int x=0; x<=3; x++)
+      for(int y=0; y<=3; y++)
+        ids.push_back(Eigen::Matrix<int,3,1>(x,y,1));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("walkerWide.obj");
+  }
+  {
+    std::shared_ptr<CellShape> cells(new CellShape());
+    std::vector<Eigen::Matrix<int,3,1>> ids;
+    for(int z=0; z<=1; z++) {
+      ids.push_back(Eigen::Matrix<int,3,1>(0,0,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(3,0,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(0,3,z));
+      ids.push_back(Eigen::Matrix<int,3,1>(3,3,z));
+    }
+    for(int x=0; x<=3; x++)
+      for(int y=0; y<=3; y++)
+        ids.push_back(Eigen::Matrix<int,3,1>(x,y,2));
+    cells->reset(ids,1);
+    drawer.addShape(cells);
+    cells->debugWriteObj("walkerHighWide.obj");
+  }
+
+#define USE_LIGHT
+#ifdef USE_LIGHT
+  drawer.addLightSystem(0);
+  drawer.getLight()->lightSz(10);
+  for(int x=-1; x<=1; x+=2)
+    for(int y=-1; y<=1; y+=2)
+      for(int z=-1; z<=1; z+=2)
+        drawer.getLight()->addLight(Eigen::Matrix<GLfloat,3,1>(x,y,z)*2,
+                                    Eigen::Matrix<GLfloat,3,1>(.2,.2,.2),
+                                    Eigen::Matrix<GLfloat,3,1>(.7,.7,.7),
+                                    Eigen::Matrix<GLfloat,3,1>(0,0,0));
+#endif
+  drawer.mainLoop();
+  return 0;
+}
--- TinyVisualizer/CameraExportPlugin.cpp
+++ TinyVisualizer/CameraExportPlugin.cpp
@@ -7,17 +7,21 @@ namespace DRAWER {
 CameraExportPlugin::CameraExportPlugin(int keySave,int keyLoad,const std::string& name):_keySave(keySave),_keyLoad(keyLoad),_fileName(name) {}
 bool CameraExportPlugin::key(GLFWwindow*,int key,int,int action,int) {
   std::shared_ptr<Camera3D> cam=std::dynamic_pointer_cast<Camera3D>(_drawer->getCamera());
-  if(!cam) {
-    std::cout << "No camera detected, cannot serialize!" << std::endl;
-    return true;
-  }
   if(key==_keySave && action==GLFW_PRESS) {
+    if(!cam) {
+      std::cout << "No camera detected, cannot serialize!" << std::endl;
+      return true;
+    }
     std::ofstream os(_fileName);
     os << cam->position()[0] << " " << cam->position()[1] << " " << cam->position()[2] << std::endl;
     os << cam->direction()[0] << " " << cam->direction()[1] << " " << cam->direction()[2] << std::endl;
     os << cam->up()[0] << " " << cam->up()[1] << " " << cam->up()[2] << std::endl;
     return false;
   } else if(key==_keyLoad && action==GLFW_PRESS) {
+    if(!cam) {
+      std::cout << "No camera detected, cannot serialize!" << std::endl;
+      return true;
+    }
     std::ifstream is(_fileName);
     Eigen::Matrix<GLfloat,3,1> v;
     is >> v[0] >> v[1] >> v[2];
