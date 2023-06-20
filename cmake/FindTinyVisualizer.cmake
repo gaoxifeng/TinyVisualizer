72432256227bf094fc4b07dd9927431de87d58b5
Ghost <9593239+pzr1988@users.noreply.github.com>
2023-06-20 13:54:50 -0700

    Update FindTinyVisualizer.cmake

--- cmake/FindTinyVisualizer.cmake
+++ cmake/FindTinyVisualizer.cmake
@@ -3,53 +3,48 @@ IF(NOT TinyVisualizer_FOUND)
 FIND_PATH(TinyVisualizer_INCLUDE_DIR TinyVisualizer/Drawer.h
   ${PROJECT_SOURCE_DIR}/include
   ${PROJECT_SOURCE_DIR}
+  $ENV{TinyVisualizer_ROOT}/include
+  $ENV{TinyVisualizer_ROOT}
   /usr/include
   /usr/local/include
-  ${TinyVisualizer_ROOT}/include
-  ${TinyVisualizer_ROOT}
+  C:/TinyVisualizer/include
   )
 
 FIND_PATH(ImGui_INCLUDE_DIR imgui/imgui.h
-  ${PROJECT_SOURCE_DIR}/extern
   ${PROJECT_SOURCE_DIR}/include
   ${PROJECT_SOURCE_DIR}
+  $ENV{TinyVisualizer_ROOT}/include
+  $ENV{TinyVisualizer_ROOT}
   /usr/include
   /usr/local/include
-  ${TinyVisualizer_ROOT}/extern
-  ${TinyVisualizer_ROOT}/include
-  ${TinyVisualizer_ROOT}
+  C:/TinyVisualizer/include
   )
 
-FIND_PATH(glad_INCLUDE_DIR glad/gl.h
-  ${PROJECT_SOURCE_DIR}/extern/glad/include
-  ${PROJECT_SOURCE_DIR}/extern/glad
-  ${PROJECT_SOURCE_DIR}/include
-  ${PROJECT_SOURCE_DIR}
-  /usr/include
-  /usr/local/include
-  ${TinyVisualizer_ROOT}/extern/glad/include
-  ${TinyVisualizer_ROOT}/extern/glad
-  ${TinyVisualizer_ROOT}/include
-  ${TinyVisualizer_ROOT}
-  )
-
-FIND_LIBRARY(TinyVisualizer_LIBRARIES NAME TinyVisualizer PATHS 
-  ${PROJECT_SOURCE_DIR}/Release
+FIND_LIBRARY(TinyVisualizer_LIBRARY NAMES TinyVisualizer PATHS 
   ${PROJECT_SOURCE_DIR}/lib
   ${PROJECT_SOURCE_DIR}
+  $ENV{TinyVisualizer_ROOT}/lib
+  $ENV{TinyVisualizer_ROOT}
   /usr/lib
   /usr/local/lib
-  ${TinyVisualizer_ROOT}/Release
-  ${TinyVisualizer_ROOT}/lib
-  ${TinyVisualizer_ROOT}
-  ${TinyVisualizer_ROOT}-build/Release
-  ${TinyVisualizer_ROOT}-build/lib
-  ${TinyVisualizer_ROOT}-build
+  C:/TinyVisualizer/lib
+  )
+  
+
+FIND_LIBRARY(glfw_LIBRARY NAMES glfw3 PATHS 
+  ${PROJECT_SOURCE_DIR}/lib
+  ${PROJECT_SOURCE_DIR}
+  $ENV{TinyVisualizer_ROOT}/lib
+  $ENV{TinyVisualizer_ROOT}
+  /usr/lib
+  /usr/local/lib
+  C:/TinyVisualizer/lib
   )
 
-IF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
+IF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARY AND glfw_LIBRARY)
   set(TinyVisualizer_FOUND TRUE)
-  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR} ${ImGui_INCLUDE_DIR} ${glad_INCLUDE_DIR})
+  set(TinyVisualizer_LIBRARIES ${TinyVisualizer_LIBRARY} ${glfw_LIBRARY})
+  set(TinyVisualizer_INCLUDE_DIRS ${TinyVisualizer_INCLUDE_DIR} ${ImGui_INCLUDE_DIR})
 ENDIF(TinyVisualizer_INCLUDE_DIR AND TinyVisualizer_LIBRARIES)
 
 MARK_AS_ADVANCED(TinyVisualizer_INCLUDE_DIR TinyVisualizer_INCLUDE_DIRS TinyVisualizer_LIBRARIES)
