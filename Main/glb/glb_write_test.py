242c721b03af372d1739b930e11e786fa2898351
zherong <zherong@cs.unc.edu>
2023-08-15 19:13:41 -0700

    tested glb io

---
+++ Main/glb/glb_write_test.py
@@ -0,0 +1,20 @@
+from loadAnimation import *
+
+if __name__=='__main__':
+    drawer = vis.Drawer(['--headless','1'])
+    anim = Animation('char10.glb')
+    for tex in anim.texs:
+        tex[:,:,:3]*=.5
+    
+    anim.save('char10Saved.glb')
+    anim = Animation('char10Saved.glb')
+    
+    render = True
+    if render:
+        use_opengl = False
+        glctx = dr.RasterizeGLContext() if use_opengl else dr.RasterizeCudaContext()
+    
+        anim.calc_animation(0, 0)
+        mvp = sample_camera_multi(anim.poss_trans, dir=np.array([0.,0.,1.]), up=np.array([0.,-1.,0.]))
+        color, depth = anim.render_by_merge(glctx, mvp, ref=False)
+        save_image('color.png', color[0,:].cpu().numpy())
