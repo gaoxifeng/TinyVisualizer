1656652068acb3098a5f071b86d5f91ae96bea8a
Ghost <zherong.pan.usa@gmail.com>
2024-02-28 01:41:02 -0800

    Update DrawerUtility.h
    Fixed bug in rtti

--- TinyVisualizer/DrawerUtility.h
+++ TinyVisualizer/DrawerUtility.h
@@ -3,12 +3,18 @@
 
 #define _USE_MATH_DEFINES // for C++
 #include <cmath>
+#include <string>
+#include <typeinfo>
 #ifdef CUSTOM_RTTI_SUPPORT
 #include <rtti/rtti.hh>
 #else
 namespace RTTI {
 class Enable {};
 #define RTTI_DECLARE_TYPEINFO(T, ...)
+template <typename T>
+std::string TypeName() {
+  return typeid(T).name();
+}
 }
 #endif
 #include <memory>
